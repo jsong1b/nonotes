9aa4642264101b2cff5092714626b43630a06b6e
jsong1b <141759463+jsong1b@users.noreply.github.com>
2025-08-26 16:07:33 -0700

    Abstract out parsing to a function

--- src/nonotes.c
+++ src/nonotes.c
@@ -1,4 +1,3 @@
-#include <assert.h>
 #include <stdio.h>
 
 #define BUF_SIZE 128
@@ -6,12 +5,44 @@
 #define NNTAG_STR_LEN 6
 const char nntag_str[NNTAG_STR_LEN] = "nntag";
 
-typedef enum ParseState {
+typedef enum ParseStateOld {
     NO_STATE,
     IN_TAG_ARGS,
     BEFORE_TAG_BODY,
     IN_TAG_BODY
+} ParseStateOld;
+
+typedef enum {
+    PEEK_AHEAD_TAGS,
+    PEEK_AHEAD_BEFORE_BODY,
+    PEEK_AHEAD_BODY
+} PeekAheadStage;
+
+typedef struct ParseState {
+    enum {
+        RULES_NOOP,
+        RULES_ARGS,
+        RULES_BODY
+    } rules_state;
+
+    char acc[BUF_SIZE];
+    int acc_idx;
+    enum {
+        ACC_NOOP,
+        ACC_COMPLETE,
+        ACC_INCOMPLETE
+    } acc_state;
+
+    FILE* fp;
+    int loc;
+
+    char buf[BUF_SIZE];
+    int buf_idx;
+
+    int bracket_level;
 } ParseState;
+
+void parseC(ParseState *parse_state, char c);
 
 
 
@@ -23,150 +54,160 @@ int main(void) {
     while ((c = fgetc(stdin)) != EOF) fputc(c, fp);
     fputc(EOF, fp);
 
-    ParseState parse_state = NO_STATE;
+    ParseStateOld parse_state_old = NO_STATE;
     char buf[BUF_SIZE] = "";
     int buf_idx = 0;
     int loc = 0;
     int tag_count = 0;
     int tag_body_level = 0;
 
+
+    ParseState parse_state = { 0 };
+    parse_state.fp = fp;
     fseek(fp, 0, 0);
     while ((c = fgetc(fp)) != EOF) {
-        loc++;
+        parseC(&parse_state, c);
 
-        buf[buf_idx] = c;
-        buf_idx = (buf_idx + 1 >= BUF_SIZE) ? 0 : buf_idx + 1;
-        buf[buf_idx] = '\0';
-
-        switch (parse_state) {
-        case NO_STATE:
-            if (c != '(') continue;
-            int tag_valid = 0;
-
-            /* check behind the "(" to see if it is preceded by "nntag" */
-            int nntag_check_idx = 0;
-            while (nntag_str[nntag_check_idx] != '\0') {
-                int peek_behind_idx = buf_idx
-                    - (NNTAG_STR_LEN + nntag_check_idx);
-                peek_behind_idx = (nntag_check_idx < 0)
-                    ? BUF_SIZE - nntag_check_idx
-                    : nntag_check_idx;
-                char peek_behind = buf[peek_behind_idx];
-
-                if (nntag_str[nntag_check_idx] != peek_behind) {
-                    tag_valid = 0;
-                    break;
-                }
-
-                tag_valid = 1;
-                nntag_check_idx++;
-            }
-
-            if (tag_valid != 1) continue;
-            tag_valid = 0;
-            /* check after the ")" to see if the tag is formatted properly */
-            ParseState peek_ahead_state = IN_TAG_ARGS;
-            int peek_ahead_level = 0;
-            char peek_ahead;
-            while ((peek_ahead = fgetc(fp)) != EOF) {
-                /* using an if tree because a switch would need goto to break
-                   out of the loop */
-                if (peek_ahead_state == IN_TAG_ARGS) {
-                    if (peek_ahead == '(') break;
-                    else if (peek_ahead == ')')
-                        peek_ahead_state = BEFORE_TAG_BODY;
-
-                    /* TODO: check if arg is longer than the buffer size */
-                } else if (peek_ahead_state == BEFORE_TAG_BODY) {
-                    if (peek_ahead == '{') peek_ahead_state = IN_TAG_BODY;
-                    else if (peek_ahead == ' '
-                        || peek_ahead == '\t'
-                        || peek_ahead == '\n')
-                    continue;
-                    else break;
-                } else if (peek_ahead_state == IN_TAG_BODY) {
-                    if (peek_ahead == '}' && peek_ahead_level <= 0) {
-                        tag_valid = 1;
-                        break;
-                    } else if (peek_ahead == '{') {
-                        peek_ahead_level++;
-                    } else if (peek_ahead == '}') {
-                        peek_ahead_level--;
-                    }
-                }
-            }
-            fseek(fp, loc, 0);
-            if (tag_valid != 1) continue;
-
-            tag_count++;
-            fprintf(stdout, "========== nntag %d ==========\ntags: [", tag_count);
-            parse_state = IN_TAG_ARGS;
-
-            break;
-        case IN_TAG_ARGS:
-            if (c == ')' || c == ',') {
-                int peek_behind_len = 2; /* 2 because buf_idx is the index for
-                                            \0, not the index for `c` */
-                int peek_behind_idx = (buf_idx - peek_behind_len < 0)
-                    ? BUF_SIZE + buf_idx - peek_behind_len
-                    : buf_idx - peek_behind_len;
-                char peek_behind = buf[peek_behind_idx];
-
-                int valid_arg = 1;
-                while (peek_behind != ',' && peek_behind != '(') {
-                    peek_behind_len++;
-                    peek_behind_idx = (buf_idx - peek_behind_len < 0)
-                        ? BUF_SIZE + buf_idx - peek_behind_len
-                        : buf_idx - peek_behind_len;
-                    peek_behind = buf[peek_behind_idx];
-                }
-                if (valid_arg != 1) {
-                    parse_state = NO_STATE;
-                    continue;
-                }
-
-                peek_behind_len--; /* remove first char from peek_behind */
-                while (peek_behind_len > 1) {
-                    int peek_behind_idx = (buf_idx - peek_behind_len < 0)
-                    ? BUF_SIZE + (buf_idx - peek_behind_len)
-                    : buf_idx - peek_behind_len;
-                    char peek_behind = buf[peek_behind_idx];
-                    peek_behind_len--;
-
-                    if (peek_behind == ' ' || peek_behind == '\t'
-                        || peek_behind == '\n') continue;
-                    fprintf(stdout, "%c", peek_behind);
-                }
-
-                if (c == ',') {
-                    fprintf(stdout, ", ");
-                } else if (c == ')') {
-                    fprintf(stdout, "]\ncontent:");
-                    parse_state = BEFORE_TAG_BODY;
-                }
-            }
-            break;
-        case BEFORE_TAG_BODY:
-            if (c == ' ' || c == '\n' || c == '\t') continue;
-            else if (c == '{') parse_state = IN_TAG_BODY;
-            else parse_state = NO_STATE;
-            break;
-        case IN_TAG_BODY:
-            if (c == '}' && tag_body_level <= 0) {
-                parse_state = NO_STATE;
-                fprintf(stdout, "\n");
-            } else {
-                if (c == '\n') fprintf(stdout, "\n  ");
-                else fprintf(stdout, "%c", c);
-
-                if (c == '{') tag_body_level++;
-                else if (c == '}') tag_body_level--;
-            }
-
-            break;
+        printf("\n==========");
+        printf("char: %c", c);
+        printf("\nfile_loc: %d", parse_state.loc);
+        printf("\nacc_idx: %d", parse_state.acc_idx);
+        printf("\nbuf_idx: %d", parse_state.buf_idx);
+        printf("\nrules: ");
+        switch (parse_state.rules_state) {
+        case RULES_NOOP: printf("noop"); break;
+        case RULES_ARGS: printf("args"); break;
+        case RULES_BODY: printf("body"); break;
         }
+        printf("\nacc state: ");
+        switch (parse_state.acc_state) {
+        case ACC_NOOP: printf("noop"); break;
+        case ACC_COMPLETE: printf("complete"); break;
+        case ACC_INCOMPLETE: printf("incomplete"); break;
+        }
+        printf("\nacc:\n%s\n", parse_state.acc);
     }
 
     fclose(fp);
     return 0;
+}
+
+void parseC(ParseState* parse_state, char c) {
+    parse_state->loc++;
+    parse_state->buf[parse_state->buf_idx] = c;
+    parse_state->buf_idx = (parse_state->buf_idx + 1 >= BUF_SIZE) ? 0 : parse_state->buf_idx + 1;
+    parse_state->buf[parse_state->buf_idx] = '\0';
+
+    switch (parse_state->rules_state) {
+    case RULES_NOOP:
+        if (c != '(') {
+            return;
+        }
+        int tag_valid = 0;
+
+        /* check behind '(' for the "nntag" string */
+        int nntag_check_idx = 0;
+        while (nntag_str[nntag_check_idx] != '\0') {
+            int peek_behind_idx = parse_state->buf_idx - (NNTAG_STR_LEN + nntag_check_idx);
+            peek_behind_idx = (nntag_check_idx < 0) ? BUF_SIZE - nntag_check_idx : nntag_check_idx;
+            char peek_behind = parse_state->buf[peek_behind_idx];
+
+            if (nntag_str[nntag_check_idx] != peek_behind) {
+                tag_valid = 0;
+                break;
+            }
+
+            tag_valid = 1;
+            nntag_check_idx++;
+        }
+        if (tag_valid != 1) {
+            return;
+        }
+        tag_valid = 0;
+
+        /* check ahead of ')' if the tag ends properly  */
+        PeekAheadStage peek_ahead_stage = PEEK_AHEAD_TAGS;
+        int peek_ahead_lvl = 0;
+        char peek_ahead = '\0';
+        while ((peek_ahead = fgetc(parse_state->fp)) != EOF) {
+            if (peek_ahead_stage == PEEK_AHEAD_TAGS) {
+                if (peek_ahead == ')') {
+                    peek_ahead_stage = PEEK_AHEAD_BEFORE_BODY;
+                } else if (peek_ahead == '(') {
+                    break;
+                }
+            } else if (peek_ahead_stage == PEEK_AHEAD_BEFORE_BODY) {
+                if (peek_ahead == '{') {
+                    peek_ahead_stage = PEEK_AHEAD_BODY;
+                } else if (peek_ahead != ' ' && peek_ahead != '\t' && peek_ahead != '\n') {
+                    break;
+                }
+            } else if (peek_ahead_stage == PEEK_AHEAD_BODY) {
+                if (peek_ahead == '}' && peek_ahead_lvl <= 0) {
+                    tag_valid = 1;
+                    break;
+                } else if (peek_ahead == '}') {
+                    peek_ahead_lvl--;
+                } else if (peek_ahead == '{') {
+                    peek_ahead_lvl++;
+                }
+            }
+        }
+        fseek(parse_state->fp, parse_state->loc, 0);
+        if (tag_valid != 1) {
+            return;
+        }
+
+        parse_state->rules_state = RULES_ARGS;
+
+        break;
+    case RULES_ARGS:
+        switch (parse_state->acc_state) {
+        case ACC_NOOP:
+            if (c == ' ' || c == '\t' || c == '\n') {
+                return;
+            }
+            parse_state->acc[0] = c;
+            parse_state->acc[1] = '\0';
+            parse_state->acc_idx = 1;
+            parse_state->acc_state = ACC_INCOMPLETE;
+
+            break;
+        case ACC_INCOMPLETE:
+            if (c == ',' || c == ')') {
+                if (c == ',') {
+                    parse_state->acc_state = ACC_NOOP;
+                } else if (c == ')') {
+                    parse_state->acc_state = ACC_COMPLETE;
+                }
+                return;
+            }
+            parse_state->acc[parse_state->acc_idx] = c;
+            parse_state->acc_idx++;
+            parse_state->acc[parse_state->acc_idx] = '\0';
+
+            break;
+        case ACC_COMPLETE:
+            if (c == '{') {
+                parse_state->rules_state = RULES_BODY;
+                parse_state->acc_idx = 0;
+                parse_state->acc_state = ACC_NOOP;
+            }
+
+            break;
+        }
+        break;
+    case RULES_BODY:
+        if (c == '{' && parse_state->bracket_level == 0) {
+            parse_state->rules_state = RULES_NOOP;
+            parse_state->acc_state = ACC_COMPLETE;
+            return;
+        } else if (c == '{') {
+            parse_state->bracket_level++;
+        } else if ('}') {
+            parse_state->bracket_level--;
+        }
+
+        break;
+    }
 }
